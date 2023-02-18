30ce2332905beae567c200ab22be1b041b7c1e3e
Varunan Sanjeepan <varunan.sanjeepan@outlook.com>
2023-02-17 17:03:02 -0800

    Added finish point

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -5,12 +5,15 @@ public class FinishPoint : NetworkBehaviour
 {
 
     public WinScreenManager manager;
+    public NetworkManager netManager;
 
     void OnCollisionEnter(Collision collision)
     {
 
         manager.AddWinScreenServerRpc(collision.gameObject.GetComponent<Move>().OwnerClientId);
         manager.AddWinScreen(collision.gameObject.GetComponent<Move>().OwnerClientId);
+
+        netManager.gameObject.SetActive(false);
 
     }
 
