a36685e875fd1a1274764fdc939b5dbbbbb2d02c
Varunan Sanjeepan <varunan.sanjeepan@outlook.com>
2023-02-17 19:57:10 -0800

    Added barrier

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -5,15 +5,12 @@ public class FinishPoint : NetworkBehaviour
 {
 
     public WinScreenManager manager;
-    public NetworkManager netManager;
 
     void OnCollisionEnter(Collision collision)
     {
 
         manager.AddWinScreenServerRpc(collision.gameObject.GetComponent<Move>().OwnerClientId);
         manager.AddWinScreen(collision.gameObject.GetComponent<Move>().OwnerClientId);
-
-        netManager.gameObject.SetActive(false);
 
     }
 
--- Assets/Scripts/WinScreenManager.cs
+++ Assets/Scripts/WinScreenManager.cs
@@ -8,6 +8,32 @@ public class WinScreenManager : NetworkBehaviour
     public GameObject winScreen;
     private GameObject spawned;
     private GameObject privateSpawned;
+    public NetworkManager netManager;
+
+    public GameObject startCanvas;
+    public GameObject barrier;
+
+    public void StartGame()
+    {
+
+        startCanvas.SetActive(false);
+        barrier.SetActive(false);
+
+    }
+
+    public override void OnNetworkSpawn()
+    {
+
+        startCanvas.SetActive(false);
+
+        if (IsHost)
+        {
+
+            startCanvas.SetActive(true);
+
+        }
+
+    }
 
     public void AddWinScreen(ulong id)
     {
@@ -25,6 +51,8 @@ public class WinScreenManager : NetworkBehaviour
         spawned = Instantiate(winScreen);
         spawned.GetComponentInChildren<TMP_Text>().text = "Player " + id.ToString() + " Has Won";
         spawned.GetComponent<NetworkObject>().Spawn(true);
+
+        netManager.gameObject.SetActive(false);
 
     }
 
